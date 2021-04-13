60cff3ec10b9d818363758d8b3e565645a375af6
Artem <44963781+T3mon@users.noreply.github.com>
2021-04-14 00:36:46 +0300

    Hotels

---
+++ DAL/Migrations/20210413213622_init.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    HotelName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RoomStatus = table.Column<int>(type: "int", nullable: false),
+                    RoomRate = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hotels");
+        }
+    }
+}
